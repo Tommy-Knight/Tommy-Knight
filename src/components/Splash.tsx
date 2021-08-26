c73da6c0909e9d55395e2a5ffb6c856dfaf47469
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-08-26 01:07:03 +0100

    Update Splash.tsx

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,25 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
 
 export const Splash = ({ history }: any) => {
 	// window.setTimeout(() => {
 	// 	history.push("/weather");
 	// }, 2000);
 
+	useEffect(() => {
+		window.setTimeout(() => {
+			console.log("🔨⏰");
+		}, 500);
+	});
+
 	const proceed = () => {
 		history.push("/weather");
-		console.log("nope");
+		console.log("Weather app");
 	};
 
 	return (
 		<>
 			<div onClick={proceed}>
 				<div className='container'>
-					<div className='glitch' data-text='TOMMY'>
+					<div className='glitch' data-text='TOMMYK'>
 						TOMMY
 					</div>
 					<div className='glow'>TOMMY</div>
 					<p className='subtitle'>
-						tommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommytommy
+						fullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdevfullstackwebdev
 					</p>
 				</div>
 				<div className='scanlines'></div>
