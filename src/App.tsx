dbe508870366c10e70367604a14d3223470188c9
Tommy Knight <tknighted@hotmail.com>
2022-06-27 13:23:03 +0100

    🐼

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import "./style/App.scss";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import React from "react";
-import Main from "./components/main";
+import './style/App.scss';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import React from 'react';
+import Main from './components/main';
 
 class App extends React.Component {
 	render() {
 		return (
 			<Router>
-				<Switch>
-					<Route path='/' render={(routeProps: any) => <Main {...routeProps} />} />
-				</Switch>
+				<Routes>
+					<Route path='/' element={<Main />} />
+				</Routes>
 			</Router>
 		);
 	}
